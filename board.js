2a5e212f4eeaea85993e8fc9ecedaee118743738
Veronica Smith <veronica.smith2009@yahoo.com>
2018-01-12 08:55:33 -0500

    incomplete board ops

--- board.js
+++ board.js
@@ -5,18 +5,18 @@
  * @param {Int} height 
  * @param {Array<Int>} cells the array to use for the cells (default: new Uint8Array(width * height))
  */
-function Board(width=32, height=32, cells) {
-  this.width = width
-  this.height = height
-  // We'll store our cells in a 1D typed array.
-  // Typed arrays are a lot like normal arrays, but they're
-  // (1) much faster, and (2) can only hold one kind of data type.
-  // In this case, we're creating a Uint8 typed array, which means
-  // it can only hold unsigned, 8-bit integers (ints from 0 to 255).
-  //
-  // Since we only really need to track 1 bit per cell, this is positively
-  // luxurious for our needs.
-  this.cells = cells || new Uint8Array(width * height)
+function Board(width = 32, height = 32, cells) {
+    this.width = width
+    this.height = height
+        // We'll store our cells in a 1D typed array.
+        // Typed arrays are a lot like normal arrays, but they're
+        // (1) much faster, and (2) can only hold one kind of data type.
+        // In this case, we're creating a Uint8 typed array, which means
+        // it can only hold unsigned, 8-bit integers (ints from 0 to 255).
+        //
+        // Since we only really need to track 1 bit per cell, this is positively
+        // luxurious for our needs.
+    this.cells = cells || new Uint8Array(width * height)
 }
 
 /**
@@ -26,18 +26,18 @@ function Board(width=32, height=32, cells) {
  * board's cells array.
  */
 Board.prototype.indexFor = function([row, col]) {
-  // OMG, a destructured parameter! ⬆️⬆️⬆️⬆️⬆️⬆️
-  //
-  // This digs into the array we were passed, plucks out the first
-  // two elements, and names them row and col. Any other elements
-  // are ignored.
-  //
-  // http://2ality.com/2015/01/es6-destructuring.html  
-  
-  // Return undefined if we're out of bounds
-  if (row < 0 || row >= this.height || col < 0 || col >= this.width)
-    return  
-  return row * this.width + col
+    // OMG, a destructured parameter! ⬆️⬆️⬆️⬆️⬆️⬆️
+    //
+    // This digs into the array we were passed, plucks out the first
+    // two elements, and names them row and col. Any other elements
+    // are ignored.
+    //
+    // http://2ality.com/2015/01/es6-destructuring.html  
+
+    // Return undefined if we're out of bounds
+    if (row < 0 || row >= this.height || col < 0 || col >= this.width)
+        return
+    return row * this.width + col
 }
 
 /**
@@ -45,8 +45,8 @@ Board.prototype.indexFor = function([row, col]) {
  * 
  * Get the value of the board at coords.
  */
-Board.prototype.get = function (coords) {
-  return this.cells[this.indexFor(coords)] || 0
+Board.prototype.get = function(coords) {
+    return this.cells[this.indexFor(coords)] || 0
 }
 
 /**
@@ -55,7 +55,9 @@ Board.prototype.get = function (coords) {
  * Set the value of the board at coords to value.
  */
 Board.prototype.set = function(coords, value) {
-  // TODO
+    // TODO
+
+    value ? this.cells[this.indexFor(coords)] = 1 : this.cells[this.indexFor(coords)] = 0;
 }
 
 /**
@@ -64,7 +66,26 @@ Board.prototype.set = function(coords, value) {
  * Return the count of living neighbors around a given coordinate.
  */
 Board.prototype.livingNeighbors = function([row, col]) {
-  // TODO: Return the count of living neighbors.
+    // TODO: Return the count of living neighbors.
+    var neighbor1 = [row - 1, col + 1];
+    var neighbor2 = [row, col + 1];
+    var neighbor3 = [row + 1, col + 1];
+    var neighbor4 = [row + 1, col];
+    var neighbor5 = [row + 1, col - 1];
+    var neighbor6 = [row, col - 1];
+    var neighbor7 = [row - 1, col - 1];
+    var neighbor8 = [row - 1, col];
+    var arr = [neighbor1, neighbor2, neighbor3, neighbor4, neighbor5, neighbor6, neighbor7, neighbor8];
+
+    var aliveCount = 0;
+    arr.forEach(neighbor => {
+        if (this.get(neighbor)) aliveCount++;
+    });
+    // arr.forEach(function(neighbor) {
+    //     //console.log(this === window);
+    //     if (this.get(neighbor).bind(this)) { aliveCount++ };
+    // });
+    return aliveCount;
 }
 
 /**
@@ -73,7 +94,7 @@ Board.prototype.livingNeighbors = function([row, col]) {
  * Toggle the cell at coords from alive to dead or vice versa.
  */
 Board.prototype.toggle = function(coords) {
-  // TODO
+    this.get(coords) ? this.set(coords, 0) : this.set(coords, 1);
 }
 
 /**
@@ -84,7 +105,15 @@ Board.prototype.toggle = function(coords) {
  * @param {Number} numLivingNeighbors 
  */
 function conway(isAlive, numLivingNeighbors) {
-  // TODO
+    if (isAlive && (numLivingNeighbors < 2 || numLivingNeighbors > 3)) {
+        return !isAlive;
+    } else if (isAlive && (numLivingNeighbors === 2 || numLivingNeighbors === 3)) {
+        return isAlive;
+    } else if (!isAlive && numLivingNeighbors === 3) {
+        return !isAlive;
+    } else {
+        return isAlive;
+    }
 }
 
 /**
@@ -95,7 +124,36 @@ function conway(isAlive, numLivingNeighbors) {
  * @param {Board!} future (is mutated)
  * @param {(Boolean, Int) -> Boolean} rules (default: conway)
  */
-function tick(present, future, rules=conway) {
-  // TODO
-  return [future, present]
+
+function tick(present, future, rules = conway) {
+    var cells = present.cells;
+
+    if (rules === conway) {
+        future.cells = cells;
+    }
+
+
+
+    if (rules === flip) {
+
+        function flip(val) {
+            if (val === 1) {
+                return 0;
+            } else if (val === 0) {
+                return 1;
+            }
+        }
+
+        var flipped = [];
+
+        for (var i = 0; i < cells.length; i++) {
+            var cell = cells[i];
+
+            flipped.push(flip(cell));
+        }
+
+        future.cells = flipped;
+    }
+
+    return [future, present]
 }
